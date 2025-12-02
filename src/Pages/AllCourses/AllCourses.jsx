86d7d3dadd088a49b851758a6583dced6fd74b5d
Abdul-Kader-Rimon <abdulkaderrimon8@gmail.com>
2025-12-02 23:50:49 +0600

    All courses  courseCard call api using axios and react-query

--- src/Pages/AllCourses/AllCourses.jsx
+++ src/Pages/AllCourses/AllCourses.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
+import Card from "./CourseCard";
+
+const fetchCourses = async () => {
+  const { data } = await axios.get("http://localhost:3000/courses");
+  return data;
+};
 
 const AllCourses = () => {
-    return (
-        <div>
-            This is all courses private page
-        </div>
-    );
+  const {
+    data: courses,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["courses"],
+    queryFn: fetchCourses,
+  });
+
+  if (isLoading) return <p className="text-center mt-5">Loading...</p>;
+    if (isError) return <p className="text-center mt-5"> Error : {error.message}</p>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {courses.map((courses) => (
+        <Card
+          key={courses._id}
+          title={courses.title}
+          image={courses.image}
+          price={courses.price}
+          category={courses.category}
+          duration={courses.duration}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default AllCourses;
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,18 @@ import { RouterProvider } from 'react-router'
 import { router } from './Router/Routes'
 import AuthProvider from './Context/AuthProvider'
 import { Toaster } from 'react-hot-toast'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
+
     <AuthProvider>
       <Toaster position='top-center' reverseOrder={ false} />
        <RouterProvider router={router}/>
     </AuthProvider>
+    </QueryClientProvider>
     
   </StrictMode>,
 )
